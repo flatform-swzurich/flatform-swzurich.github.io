7d1be07ad34e5e68bbe4664c0c70a2d0e5cf6188
Iwan Gulenko <igu@polynorm.ch>
2015-09-26 01:57:07 +0200

    i

---
+++ js/misc.js
@@ -0,0 +1,15 @@
+   	
+    	$(document).ready(function(){
+        
+		$('.autoplay').slick({
+		  slidesToShow: 1,
+		  slidesToScroll: 1,
+		  autoplay: true,
+		  autoplaySpeed: 1500,
+		  dots:true,
+		  pauseOnHover:false,
+		  pauseOnDotsHover:true 
+		});
+
+
+    });
