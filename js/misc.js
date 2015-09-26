00a61e7091ec40850d78c510b7181fdbd2201ef5
Iwan Gulenko <igu@polynorm.ch>
2015-09-26 13:48:54 +0200

    aweomse

--- js/misc.js
+++ js/misc.js
@@ -5,7 +5,7 @@
 		  slidesToShow: 1,
 		  slidesToScroll: 1,
 		  autoplay: true,
-		  autoplaySpeed: 1500,
+		  autoplaySpeed: 2500,
 		  dots:true,
 		  pauseOnHover:false,
 		  pauseOnDotsHover:true 
